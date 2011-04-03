f0c61ad1903bdb9304b811f199c110d8943a3c8c
Manel <manuel.roma01@gmail.com>
2011-04-03 16:40:14 +0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackupManager::Application.config.secret_token = 'd7a063ca676944ec732b2350fd255104762cdf38b8282ffa7c6fd09fe50c63991e0536dc3414f30cc9b3d24d22d8bb92cff3dfc4f5de7d7a8867d70b0e4ef01b'
